fd13f6b9fed83a854b97bf7ac909856f9a48bc22
Jawad-Hussain-dev <bejetstech@gmail.com>
2025-07-16 19:21:07 +0500

    Update explain_prediction.py

--- explain_prediction.py
+++ explain_prediction.py
@@ -16,22 +16,22 @@ def explain_prediction(model, image_path, label_map):
     features = np.array(features).reshape(1, -1)
 
     pred = model.predict(features)[0]
-    print(f"\n🧠 Predicted label: {pred} (Person folder: {reverse_map[pred]})")
+    print(f"\n Predicted label: {pred} (Person folder: {reverse_map[pred]})")
 
     node_indicator = model.decision_path(features)
     leaf_id = model.apply(features)
 
-    print("\n🔎 Decision path:")
+    print("\n Decision path:")
     for node_id in node_indicator.indices:
         if leaf_id[0] == node_id:
-            print(f"👉 Reached leaf node {node_id}")
+            print(f" Reached leaf node {node_id}")
         else:
             feature = model.tree_.feature[node_id]
             threshold = model.tree_.threshold[node_id]
             decision = "<=" if features[0, feature] <= threshold else ">"
             print(f" - Feature f{feature} ({decision} {threshold:.2f})")
 
-    print("\n📊 Top 5 most important features:")
+    print("\n Top 5 most important features:")
     importances = model.feature_importances_
     top_indices = np.argsort(importances)[-5:][::-1]
     for idx in top_indices:
