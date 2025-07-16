bd6b53cabd4311e6538635169e7f165586fb4863
Jawad-Hussain-dev <bejetstech@gmail.com>
2025-07-16 19:22:17 +0500

    Update train2.py

--- train2.py
+++ train2.py
@@ -9,7 +9,7 @@ X, y, label_map = build_dataset(dataset_path)
 model = train_decision_tree(X, y, max_depth=10)
 
 joblib.dump((X, y, label_map), "hog_dataset.pkl")
-print("✅ Dataset saved to hog_dataset.pkl")
+print(" Dataset saved to hog_dataset.pkl")
 
 joblib.dump(model, "tree_model.pkl")
-print("✅ Model saved to tree_model.pkl")
+print(" Model saved to tree_model.pkl")
